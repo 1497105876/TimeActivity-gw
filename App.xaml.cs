b04d0be7145654645fe4fd2165d4c3d5a1f11d11
gw <1497105876@qq.com>
2026-07-18 20:49:18 +0800

    初始化

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TimeActivity;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
